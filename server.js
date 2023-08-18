7a777c328169cd1074013bffb2853335218eb056
lchen104 <lchen@zdmedia.com>
2023-08-17 21:07:27 -0400

    Added ancor link  to remove a bottle

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const express = require('express');
 const app = express();
 const port = 5001;
 
+
 app.get("/", (req, res) => {
     res.send(`<h1>99 Bottles of beer on the wall</h1>
-    <a href='http://localhost:5001/98'>Take one down, pass it around</a>`);
+    <a href='http://localhost:5001/98'>Take one down, Pass it around</a>`);
   });
 
   
-  app.get("/:bottlenumber", (req, res) => {
+  app.get("/:numBottle", (req, res) => {
+    const nextNumber = req.params.numBottle - 1;
 
+    if (nextNumber >= 0) {
+        res.send(`<h1>${req.params.numBottle} bottle(s) of beer on the wall</h1>
+        <a href='http://localhost:5001/${nextNumber}'>Take one down, Pass it around</a>`);
+    } else {
+      res.send(`<a href=//localhost:5001>Home Page</a>`);
+    }
   });
 
 
