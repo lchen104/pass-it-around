5ad98de1a376116f0cfb415486cc0643ee81c6bf
lchen104 <lchen@zdmedia.com>
2023-08-17 20:50:06 -0400

    Added basic Express Structure

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 // pass it around
 
+const express = require('express');
+const app = express();
+const port = 5001;
+
+app.get("/", (req, res) => {
+    res.send();
+  });
+
+  
+  app.get("/:bottlenumber", (req, res) => {
+    
+  });
+
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
